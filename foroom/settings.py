542e2dc791d3a7b5fa272a8c642be2ef212693b6
lionzxy <nikita@kulikof.ru>
2018-01-19 18:43:59 +0300

    Change setting

--- foroom/settings.py
+++ foroom/settings.py
@@ -5,11 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 
 database_auth_params = {
-    'database': 'technopark',
-    'user': 'lionzxy',
+    'database': 'docker',
+    'user': 'docker',
     'host': 'localhost',
     'port': 5432,
-    'password': '123456789',
+    'password': 'docker',
 }
 
 connection = postgresql.open(**database_auth_params)
